286d90575ca9c298d4f3ecbe14598070ccd6f261
pbyenstuen <pl4nders@gmail.com>
2023-12-03 01:34:15 +0100

    add start script

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { execSync } from 'child_process';
+
+const specificDate = parseInt(process.argv[2]);
+const now = new Date();
+
+if (!specificDate && (now.getMonth() !== 11 || now.getDate() > 24)) {
+  console.log('It is currently not Advent season. Please specify a date within Advent season (npm start <1-24>)');
+  process.exit(1);
+}
+
+if (!isNaN(specificDate) && (specificDate < 1 || specificDate > 24)) {
+  console.log('Please provide a date within the Advent season (npm start <1-24>)');
+  process.exit(1);
+}
+
+if (isNaN(specificDate)) {
+  console.log('Please provide a date within the Advent season (npm start <1-24>)');
+  process.exit(1);
+}
+
+const currentModulePath = fileURLToPath(import.meta.url);
+const currentDir = path.dirname(currentModulePath);
+
+const date = specificDate
+  ? specificDate
+  : now.getDate();
+
+const folderName = `december-${date}`;
+const folderPath = path.join(currentDir, folderName);
+const indexPath = path.join(folderPath, 'index.js');
+
+if (!fs.existsSync(folderPath)) {
+  fs.mkdirSync(folderPath);
+  fs.writeFileSync(indexPath, '');
+} else {
+  console.log(`Running ${folderName}`)
+  execSync(`node src/${folderName}`, { stdio: 'inherit' });
+}
