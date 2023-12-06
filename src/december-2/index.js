325ff0227b9a35031f44dc5c72ca0dbfdbd2927c
pbyenstuen <pl4nders@gmail.com>
2023-12-06 01:10:37 +0100

    improve dec 2

--- src/december-2/index.js
+++ src/december-2/index.js
@@ -1,65 +1,80 @@
 import { input } from "./input.js";
 
-const gamesArr = input.split('\n');
+const gameStrings = input.split('\n');
 
-const getSum = (numsArr) => numsArr.reduce((sum, cur) => sum + cur);
-const getProduct = (numsArr) => numsArr.reduce((product, cur) => product * cur);
+const parseGames = gameStrings => {
+  return gameStrings.map(gameStr => {
+    const nr = parseInt(gameStr.split(':')[0].replace('Game ', ''));
+    const setStrings = gameStr.split(': ')[1].split('; ');
 
-const solvePartOne = (gamesArr) => {
-  const bag = {
+    const sets = setStrings.map(setStr => {
+      const colorCountMaps =
+        setStr.split(', ').map(setStr => setStr.split(' '));
+
+      return colorCountMaps.reduce((set, [count, color]) =>
+        Object.assign(set, { [color]: parseInt(count) }), {});
+    });
+
+    return { nr, sets };
+  })
+}
+
+const getSum = numbers =>
+  numbers.reduce((sum, num) => sum + num);
+
+const getProduct = numbers =>
+  numbers.reduce((product, num) => product * num);
+
+const isSetPossible = (set, bagOfCubes) =>
+  Object.entries(set).every(([color, count]) =>
+    bagOfCubes[color] >= count);
+
+const isGamePossible = (game, bagOfCubes) =>
+  game.sets.every(set => isSetPossible(set, bagOfCubes));
+
+const getPossibleGames = (games, bagOfCubes) => games
+  .filter(game => isGamePossible(game, bagOfCubes))
+  .map(game => game.nr);
+
+const getMinPossibleBagOfCubes = game => {
+  return game.sets.reduce((bag, set) => {
+    Object.entries(set).forEach(([color, count]) => {
+      bag[color] = Math.max(bag[color], parseInt(count));
+    });
+
+    return bag;
+  }, {
+    red: 0,
+    green: 0,
+    blue: 0,
+  });
+};
+
+const solvePartOne = gameStrings => {
+  const bagOfCubes = {
     red: 12,
     green: 13,
     blue: 14,
   };
 
-  const possibleGames = gamesArr.map(game => {
-    const id = parseInt(game.split(':')[0].replace('Game ', ''));
-    const sets = game.split(': ')[1];
-    const colorPicks = sets.replaceAll(';', ',').split(', ');
-
-    const isPossible = colorPicks.every(pick => {
-      const [count, color] = pick.split(' ');
-
-      return bag[color] >= count;
-    })
-
-    return isPossible ? id : null;
-  }).filter(id => id);
+  const games = parseGames(gameStrings);
+  const possibleGames = getPossibleGames(games, bagOfCubes);
 
   return getSum(possibleGames);
 }
 
-const solvePartTwo = (gamesArr) => {
-  const minBags = gamesArr.map(game => {
-    const sets = game.split(': ')[1];
-    const colorPicks = sets.replaceAll(';', ',').split(', ');
+const solvePartTwo = gameStrings => {
+  const games = parseGames(gameStrings);
+  const minBags = games.map(game =>
+    getMinPossibleBagOfCubes(game));
 
-    const minBag = {
-      red: 0,
-      green: 0,
-      blue: 0,
-    };
-
-    colorPicks.forEach(pick => {
-      const [count, color] = pick.split(' ');
-      const parsedCount = parseInt(count);
-
-      if (minBag[color] < parsedCount) {
-        minBag[color] = parsedCount;
-      }
-    });
-
-    return minBag;
-  });
-
-  const powers = minBags.map(bag => getProduct(Object.values(bag)));
-
-  return getSum(powers);
-}
+  return getSum(minBags.map(bag =>
+    getProduct(Object.values(bag))));
+};
 
 const answers = {
-  1: solvePartOne(gamesArr),
-  2: solvePartTwo(gamesArr),
+  1: solvePartOne(gameStrings),
+  2: solvePartTwo(gameStrings),
 };
 
 console.log(answers);
