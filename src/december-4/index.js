229df4800fffedebff4779f616a7c4cecb682b39
pbyenstuen <pl4nders@gmail.com>
2023-12-17 02:58:56 +0100

    solve dec 4 part 2

--- src/december-4/index.js
+++ src/december-4/index.js
@@ -9,9 +9,9 @@ const parseStrings = stringsArr =>
   stringsArr.map(str => parseInt(str));
 
 const parseCards = cardStrings => {
-  return cardStrings.map(cardString => {
-    const nr = cardString.split(':')[0].split(' ')[1];
-    const numbersString = cardString.split(':')[1];
+  return cardStrings.map(cardStr => {
+    const nr = cardStr.split(':')[0].split('Card')[1].trim();
+    const numbersString = cardStr.split(':')[1];
     const winningNumbers = numbersString.split('|')[0].split(' ');
     const myNumbers = numbersString.split('|')[1].split(' ');
 
@@ -22,26 +22,55 @@ const parseCards = cardStrings => {
     }
   })
 }
+const calculateCardValue = card =>
+  card.myNumbers.reduce((value, num) =>
+    card.winningNumbers.includes(num) ? (value === 0 ? 1 : value * 2) : value, 0);
 
-const calculateCardValue = ({ winningNumbers, myNumbers }) => {
-  return myNumbers.reduce((value, num) => {
-    if (winningNumbers.includes(num)) {
-      return value === 0 ? 1 : value * 2;
-    } else {
-      return value;
-    }
-  }, 0)
+const getMatchingNumbersCount = card =>
+  card.myNumbers.reduce((count, num) =>
+    card.winningNumbers.includes(num) ? count + 1 : count, 0)
+
+const createRange = (start, length) => {
+  const range = [];
+
+  for (let i = 0; i < length; i++) {
+    range.push(start + i);
+  }
+
+  return range;
 }
 
 const solvePartOne = cardStrings => {
   const cards = parseCards(cardStrings);
-  const cardValues = cards.map(card => calculateCardValue(card))
+  const cardValues = cards.map(card =>
+    calculateCardValue(card))
 
-  return cardValues.reduce((sum, cur) => sum + cur);
+  return cardValues.reduce((sum, num) => sum + num);
+}
+
+const solvePartTwo = cardStrings => {
+  const parsedCards = parseCards(cardStrings);
+  let cards = [...parsedCards];
+  let cardsCount = cards.length;
+
+  while (cards.length) {
+    const wonCards = [];
+
+    for (const card of cards) {
+      const wonCardNrs = createRange(card.nr + 1, getMatchingNumbersCount(card));
+      wonCards.push(...parsedCards.filter(card => wonCardNrs.includes(card.nr)));
+    }
+
+    cards = wonCards;
+    cardsCount += cards.length;
+  }
+
+  return cardsCount;
 }
 
 const answers = {
   1: solvePartOne(cardStrings),
+  2: solvePartTwo(cardStrings),
 }
 
 console.log(answers);
