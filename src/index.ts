1827c3a322f43e440f3e8f0568db893cc323d207
pbyenstuen <pl4nders@gmail.com>
2024-12-01 14:58:25 +0100

    typescript for 2024 and beyond

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import path from "path";
 import fs from "fs";
-import { fileURLToPath } from "url";
 import { execSync } from "child_process";
 
 const [dayArg, yearArg] = process.argv.slice(2);
@@ -10,7 +9,7 @@ const currentDay = now.getUTCDate();
 const currentMonth = now.getUTCMonth();
 const currentYear = now.getUTCFullYear();
 
-const validateDay = (day) => {
+const validateDay = (day: string): string => {
   const parsedDay = parseInt(day, 10);
 
   if (isNaN(parsedDay) || parsedDay < 1 || parsedDay > 24) {
@@ -23,7 +22,7 @@ const validateDay = (day) => {
   return parsedDay.toString();
 };
 
-const validateYear = (year) => {
+const validateYear = (year: string): string => {
   const parsedYear = parseInt(year, 10);
 
   if (isNaN(parsedYear) || parsedYear > currentYear) {
@@ -40,19 +39,15 @@ if (!dayArg && (currentMonth !== 11 || currentDay > 24)) {
   console.error(
     "It is currently not Advent season. Please specify a day within Advent season (npm start <1-24>)."
   );
-
   process.exit(1);
 }
 
 const day = dayArg ? validateDay(dayArg) : currentDay.toString();
 const year = yearArg ? validateYear(yearArg) : currentYear.toString();
 
-const currentModulePath = fileURLToPath(import.meta.url);
-const currentDir = path.dirname(currentModulePath);
-
-const folderPath = path.join(currentDir, year, day);
+const folderPath = path.join("src", year, day);
 const inputPath = path.join(folderPath, "input.txt");
-const indexPath = path.join(folderPath, "index.js");
+const indexPath = path.join(folderPath, "index.ts");
 
 try {
   if (!fs.existsSync(folderPath)) {
@@ -60,15 +55,15 @@ try {
 
     const indexBoilerplate = `import { readInputFile } from "../../utils.js";
 
-const input = readInputFile(import.meta.url);
+const input = readInputFile(${year}, ${day});
 `;
 
     fs.writeFileSync(inputPath, "");
     fs.writeFileSync(indexPath, indexBoilerplate);
     console.log(`Created boilerplate for day ${day} of ${year}`);
   } else {
-    console.log(`Running day ${day} of ${year}`);
-    execSync(`node src/${year}/${day}`, { stdio: "inherit" });
+    console.log(`Running day ${day} of ${year} \n`);
+    execSync(`node dist/${year}/${day}`, { stdio: "inherit" });
   }
 } catch (error) {
   console.error("Error creating or running folder:", error);
---
+++ src/utils.ts
@@ -0,0 +1,21 @@
+import fs from "fs";
+import path from "path";
+
+/**
+ * Reads the content of the `input.txt` file for a given year and day.
+ * The file is expected to be located under the `src/{year}/{day}/input.txt` directory.
+ *
+ * @param {number} year - The year (e.g., 2024) of the Advent day folder.
+ * @param {number} day - The day (e.g., 1) of the Advent season, within the year folder.
+ * @returns {string|null} The content of the `input.txt` file, or `null` if the file doesn't exist or an error occurs.
+ */
+export const readInputFile = (year: number, day: number): string | null => {
+  const inputFilePath = path.join("src", `${year}`, `${day}`, "input.txt");
+
+  try {
+    return fs.readFileSync(inputFilePath, "utf-8");
+  } catch (error) {
+    console.error("Error reading input.txt:", error);
+    return null;
+  }
+};
