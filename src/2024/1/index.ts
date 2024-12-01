bed70d22edadcdf8a29e9701fea72db7c9be8a18
pbyenstuen <pl4nders@gmail.com>
2024-12-01 20:11:33 +0100

    solve 2024 day 1 part 2

--- src/2024/1/index.ts
+++ src/2024/1/index.ts
@@ -8,37 +8,46 @@ type Lists = {
 };
 
 const parseLists = (input: string): Lists => {
-  const numbers = input
-    .split("\n")
-    .map((str) => str.split("   "))
-    .flat()
-    .map((str) => parseInt(str, 10));
+  const nums = input.split("\n").map((line) => line.split("   ").map(Number));
 
-  return numbers.reduce<{ left: number[]; right: number[] }>(
-    (acc, cur, index) =>
-      index % 2 === 0
-        ? { ...acc, left: [...acc.left, cur] }
-        : { ...acc, right: [...acc.right, cur] },
-    { left: [], right: [] }
-  );
+  return {
+    left: nums.map(([leftNum]) => leftNum),
+    right: nums.map(([, rightNum]) => rightNum),
+  };
 };
 
-const solvePartOne = (input: string): number => {
-  const { left, right } = parseLists(input);
+const lists = parseLists(input);
+
+const solvePartOne = (lists: Lists): number => {
+  const { left, right } = lists;
 
   const sortedLeft = left.toSorted((a, b) => a - b);
   const sortedRight = right.toSorted((a, b) => a - b);
 
-  const distances = sortedLeft.map((_, index: number) =>
-    Math.abs(sortedLeft[index] - sortedRight[index])
+  return sortedLeft
+    .map((_, i: number) => Math.abs(sortedLeft[i] - sortedRight[i]))
+    .reduce((acc, cur) => acc + cur, 0);
+};
+
+const solvePartTwo = (lists: Lists): number => {
+  const { left, right } = lists;
+
+  const rightCounts = right.reduce<Record<number, number>>(
+    (acc, num) => ({
+      ...acc,
+      [num]: (acc[num] || 0) + 1,
+    }),
+    {}
   );
 
-  return distances.reduce((acc, cur) => acc + cur);
+  return left
+    .map((num) => num * (rightCounts[num] || 0))
+    .reduce((acc, cur) => acc + cur, 0);
 };
 
 const answers = {
-  1: solvePartOne(input),
-  2: null,
+  1: solvePartOne(lists),
+  2: solvePartTwo(lists),
 };
 
 console.log(answers);
