2c440b354587a525dcc14eb649bf0fcfd8eb4733
pbyenstuen <pl4nders@gmail.com>
2024-12-01 16:58:03 +0100

    adjust file reading func

--- src/utils.ts
+++ src/utils.ts
@@ -7,15 +7,15 @@ import path from "path";
  *
  * @param {number} year - The year (e.g., 2024) of the Advent day folder.
  * @param {number} day - The day (e.g., 1) of the Advent season, within the year folder.
- * @returns {string|null} The content of the `input.txt` file, or `null` if the file doesn't exist or an error occurs.
+ * @returns {string|null} The content of the `input.txt` file, or empty string if the file doesn't exist or an error occurs.
  */
-export const readInputFile = (year: number, day: number): string | null => {
+export const readInputFile = (year: number, day: number): string => {
   const inputFilePath = path.join("src", `${year}`, `${day}`, "input.txt");
 
   try {
     return fs.readFileSync(inputFilePath, "utf-8");
   } catch (error) {
     console.error("Error reading input.txt:", error);
-    return null;
+    return "";
   }
 };
