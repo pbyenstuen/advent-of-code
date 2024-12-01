c81bd9bc991e575d266c5b8d9b59fd6b467d3bf1
pbyenstuen <pl4nders@gmail.com>
2024-12-01 13:17:29 +0100

    improve start script

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 #### Advent of Code 2023
 
 ##### How to run
+
 1. `nvm use` to get latest JS features
-2. `npm start` to run current day <br>
-`npm start <1-24>` to run specific day
+2. `npm start` to run current day in the current year<br>
+   `npm start <1-24>` to run specific day in the current year<br>
+   `npm start <1-24> <year>` to run specific day in a specific year
--- src/index.js
+++ src/index.js
@@ -1,43 +1,76 @@
-import path from 'path';
-import fs from 'fs';
-import { fileURLToPath } from 'url';
-import { execSync } from 'child_process';
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { execSync } from "child_process";
 
-const specificDate = process.argv[2];
+const [dayArg, yearArg] = process.argv.slice(2);
+
 const now = new Date();
+const currentDay = now.getUTCDate();
+const currentMonth = now.getUTCMonth();
+const currentYear = now.getUTCFullYear();
 
-if (!specificDate && (now.getMonth() !== 11 || now.getDate() > 24)) {
-  console.log('It is currently not Advent season. Please specify a date within Advent season (npm start <1-24>)');
+const validateDay = (day) => {
+  const parsedDay = parseInt(day, 10);
+
+  if (isNaN(parsedDay) || parsedDay < 1 || parsedDay > 24) {
+    console.error(
+      "Invalid day. Please provide a day between 1 and 24 (npm start <1-24>)."
+    );
+    process.exit(1);
+  }
+
+  return parsedDay.toString();
+};
+
+const validateYear = (year) => {
+  const parsedYear = parseInt(year, 10);
+
+  if (isNaN(parsedYear) || parsedYear > currentYear) {
+    console.error(
+      `Invalid year. Please provide a year between ${minYear} and ${currentYear} (npm start <day> <year>).`
+    );
+    process.exit(1);
+  }
+
+  return parsedYear.toString();
+};
+
+if (!dayArg && (currentMonth !== 11 || currentDay > 24)) {
+  console.error(
+    "It is currently not Advent season. Please specify a day within Advent season (npm start <1-24>)."
+  );
+
   process.exit(1);
 }
 
-if (specificDate && !isNaN(specificDate) && (specificDate < 1 || specificDate > 24)) {
-  console.log('Please provide a date within the Advent season (npm start <1-24>)');
-  process.exit(1);
-}
-
-if (specificDate && isNaN(specificDate)) {
-  console.log('Please provide a date within the Advent season (npm start <1-24>)');
-  process.exit(1);
-}
+const day = dayArg ? validateDay(dayArg) : currentDay.toString();
+const year = yearArg ? validateYear(yearArg) : currentYear.toString();
 
 const currentModulePath = fileURLToPath(import.meta.url);
 const currentDir = path.dirname(currentModulePath);
 
-const date = specificDate
-  ? specificDate
-  : now.getDate();
+const folderPath = path.join(currentDir, year, day);
+const inputPath = path.join(folderPath, "input.txt");
+const indexPath = path.join(folderPath, "index.js");
 
-const folderName = `december-${date}`;
-const folderPath = path.join(currentDir, folderName);
-const indexPath = path.join(folderPath, 'index.js');
-const inputPath = path.join(folderPath, 'input.js');
+try {
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath, { recursive: true });
 
-if (!fs.existsSync(folderPath)) {
-  fs.mkdirSync(folderPath);
-  fs.writeFileSync(indexPath, '');
-  fs.writeFileSync(inputPath, '');
-} else {
-  console.log(`Running ${folderName}`)
-  execSync(`node src/${folderName}`, { stdio: 'inherit' });
+    const indexBoilerplate = `import { readInputFile } from "../../utils.js";
+
+const input = readInputFile(import.meta.url);
+`;
+
+    fs.writeFileSync(inputPath, "");
+    fs.writeFileSync(indexPath, indexBoilerplate);
+    console.log(`Created boilerplate for day ${day} of ${year}`);
+  } else {
+    console.log(`Running day ${day} of ${year}`);
+    execSync(`node src/${year}/${day}`, { stdio: "inherit" });
+  }
+} catch (error) {
+  console.error("Error creating or running folder:", error);
+  process.exit(1);
 }
